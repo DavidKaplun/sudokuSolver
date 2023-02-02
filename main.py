0b9e797cd19074c46366d34170954bfb75f5a7d3
David <davidkaplun505@gmail.com>
2023-02-02 08:01:48 +0200

    created all the functions but they dont work properly yet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 
-
-
 NUM_OF_THREE_BY_THREE_SQUARES=9
 
 UNCHECKED=0
@@ -10,8 +8,10 @@ FILLED=2
 HEIGHT=9
 WIDTH=9
 
+EMPTY=0
+
 def main():
-    sudoku_solver([1,1,2,2,2,3]).solve()
+    print(sudoku_solver([[9,1,7,2,5,4,0,0,0],[4,0,2,0,8,0,0,0,0],[6,5,0,0,0,3,4,0,0],[0,0,3,0,9,0,2,5,6],[5,0,0,7,0,0,3,0,9],[2,0,0,0,0,5,0,7,1],[0,2,0,5,3,0,7,6,0],[3,7,0,1,6,0,0,9,8],[0,0,0,0,0,0,0,3,0]]).solve())
 
 
 class sudoku_solver:
@@ -24,21 +24,32 @@ class sudoku_solver:
     # 2 means the number is in the square
     squares={0:[0,0,0,0,0,0,0,0,0],1:[0,0,0,0,0,0,0,0,0],2:[0,0,0,0,0,0,0,0,0],3:[0,0,0,0,0,0,0,0,0],4:[0,0,0,0,0,0,0,0,0],5:[0,0,0,0,0,0,0,0,0],6:[0,0,0,0,0,0,0,0,0],7:[0,0,0,0,0,0,0,0,0],8:[0,0,0,0,0,0,0,0,0]}
 
+    is_filled=True
+
     def __init__(self,matrix):
         self.matrix=matrix
 
     def solve(self):
         self.count_how_many_times_each_number_appears()
         self.sort_count_of_nums()
-        self.find_the_next_common_num(0)
-        print(self.most_common_num)
+        print(self.count_of_nums)
+        for n in range(9):
+            self.find_the_next_common_num(n)
+            if(self.count_of_nums[self.most_common_num]<9):
+                is_filled = True
+                while(self.is_filled==True):
+                    self.is_filled = False
+                    self.fill_grid_with_num(self.most_common_num)
+
+        print(self.count_of_nums)
+        print(self.matrix)
 
 
     def fill_grid_with_num(self,number):
         #this function will check each 3x3 square if it has this number
         #and if not then it will try to fill it
         for key in range(NUM_OF_THREE_BY_THREE_SQUARES):
-            if(self.squares[key][number-1]==UNCHECKED):#if the flag is 0 it means the square is unchecked for this number
+            if(self.squares[key][number-1]==UNCHECKED):
                 self.check_small_squares_avaliable_for_num_in_square(key,number)
 
     def check_small_squares_avaliable_for_num_in_square(self,square,number):
@@ -48,15 +59,22 @@ class sudoku_solver:
         possible_places_to_put_number=[]
         for row in rows:
             for column in columns:
-                if(self.matrix[row][column]==0):
+                if(self.matrix[row][column]==EMPTY):
                     possible_places_to_put_number.append([row,column])
 
-        if(len(possible_places_to_put_number)==1):
+        if(len(possible_places_to_put_number)==1):#check if there is only one option for the number
             self.matrix[possible_places_to_put_number[0][0]][possible_places_to_put_number[0][1]]=number#putting the number in the matrix
             self.squares[square][number-1]=FILLED
+            self.count_of_nums[number]+=1
+            self.is_filled=True
         else:
             self.squares[square][number-1]=CHECKED
-        
+
+    def reset_all_checked_to_unchecked(self,number):
+        for square_key in range(NUM_OF_THREE_BY_THREE_SQUARES):
+            if(self.squares[square_key][number-1]==CHECKED):
+                self.squares[square_key][number-1]==UNCHECKED
+
 
 
     def find_available_rows_for_number(self, number, square):
@@ -93,11 +111,12 @@ class sudoku_solver:
 
     def count_how_many_times_each_number_appears(self):
         count=0
-        for number in self.matrix:
-            if (number != 0):
-                self.count_of_nums[number] += 1
-                self.squares[self.get_num_of_square(count)][number-1]=FILLED#2 is flag for filled
-            count+=1
+        for row in self.matrix:
+            for number in row:
+                if (number != 0):
+                    self.count_of_nums[number] += 1
+                    self.squares[self.get_num_of_square(count)][number-1]=FILLED#2 is flag for filled
+                count+=1
 
 
     def get_num_of_square(self,index):
